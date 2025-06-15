2d5fbbad6d101ea6ac899b7a7729ed1575b46701
Antonynns <nunesantonio1111@gmail.com>
2025-06-15 14:18:26 -0300

    CRUD Aluno (update nao funciona)

--- controllers/alunoController.js
+++ controllers/alunoController.js
@@ -1,18 +1,82 @@
-const AlunoModel = require('../models/Aluno');
+const bcryptjs = require("bcryptjs");
+const AlunoModel = require("../models/Aluno");
 
 class AlunoController {
-    static async listar(req, res) {
-        if (req.query.acao === 'excluir' && req.query.matricula) {
-            await AlunoModel.deleteOne({ matricula: req.query.matricula });
-        }
 
-        const alunos = await AlunoModel.find();
-        res.render("alunos", { alunos });
+    // Listar todos os alunos
+    static async listar(req, res) {
+        try {
+            // Se houver query de exclusão
+            if (req.query.acao === 'excluir' && req.query.id) {
+                await AlunoModel.deleteOne({ _id: req.query.id });
+            }
+
+            const alunos = await AlunoModel.find();
+            res.render("alunos/listar", { alunos });
+        } catch (error) {
+            console.error("Erro ao listar alunos:", error);
+            res.status(500).send("Erro ao listar alunos");
+        }
     }
 
+    // Exibir detalhes de um aluno
     static async detalhar(req, res) {
-        const aluno = await AlunoModel.findById(req.params.id);
-        res.render("detalhar", { aluno });
+        try {
+            const aluno = await AlunoModel.findById(req.params.id);
+            if (!aluno) {
+                return res.status(404).send("Aluno não encontrado");
+            }
+            res.render("alunos/detalhar", { aluno });
+        } catch (error) {
+            console.error("Erro ao detalhar aluno:", error);
+            res.status(500).send("Erro ao detalhar aluno");
+        }
+    }
+
+    // Exibir formulário de cadastro de novo aluno
+    static cadastrarGet(req, res) {
+        res.render("alunos/cadastrar", { aluno: { nome: "", matricula: "", disciplina: "" } });
+    }
+
+    // Exibir formulário de atualização de aluno existente
+    static async atualizar(req, res) {
+        try {
+            const aluno = await AlunoModel.findById(req.params.id);
+            if (!aluno) {
+                return res.status(404).send("Aluno não encontrado");
+            }
+            res.render("alunos/cadastrar", { aluno });
+        } catch (error) {
+            console.error("Erro ao carregar formulário de edição:", error);
+            res.status(500).send("Erro ao carregar formulário");
+        }
+    }
+
+    // Processar cadastro ou atualização (POST)
+    static async cadastrar(req, res) {
+        try {
+            const { _id, nome, matricula, disciplina } = req.body;
+            if (!_id) {
+                const novoAluno = new AlunoModel({
+                    nome,
+                    matricula,
+                    disciplina
+                });
+                await novoAluno.save();
+            } else {
+                // Atualização de aluno existente
+                await AlunoModel.findByIdAndUpdate(_id, {
+                    nome,
+                    matricula,
+                    disciplina
+                });
+            }
+
+            res.redirect("/alunos");
+        } catch (error) {
+            console.error("Erro ao salvar aluno:", error);
+            res.status(500).send("Erro ao salvar aluno");
+        }
     }
 }
 
--- routes/alunoRoutes.js
+++ routes/alunoRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const AlunoController = require('../controllers/AlunoController');
+const AlunoController = require('../controllers/alunoController');
 
 router.get('/', AlunoController.listar);
-router.get('/detalhar/:id', AlunoController.detalhar);
-
+router.get('/:id', AlunoController.detalhar);
+router.get('/cadastrar', AlunoController.cadastrarGet);
+router.get('/cadastrar/:id', AlunoController.atualizar);
+router.post('/cadastrar', AlunoController.cadastrar);
 
 module.exports = router;
